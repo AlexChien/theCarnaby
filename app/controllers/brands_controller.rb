05d590867af0f81e308858e8f785ee0efe78e365
lijian <jj365952505@hotmail.com>
2010-10-11 01:47:09 +0800

    brand error messages and paginate

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,71 +1,41 @@
 class BrandsController < ApplicationController
-  # GET /brands
-  # GET /brands.xml
+
   def index
     @brand = Brand
     if params[:search]
       @brand = @brand.name_contain(params[:search][:name]) unless params[:search][:name].blank?
       @brand = @brand.description_contain(params[:search][:description]) unless params[:search][:description].blank?
     end
-    @brands = @brand.all(:order=>"created_at DESC")
+    @brands = @brand.paginate(:all,:page => params[:page],:per_page=>15,:order=>"created_at DESC")
   end
 
-  # GET /brands/1
-  # GET /brands/1.xml
   def show
     @brand = Brand.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @brand }
-    end
   end
 
-  # GET /brands/new
-  # GET /brands/new.xml
   def new
     @brand = Brand.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @brand }
-    end
   end
 
-  # GET /brands/1/edit
   def edit
     @brand = Brand.find(params[:id])
   end
 
-  # POST /brands
-  # POST /brands.xml
   def create
     @brand = Brand.new(params[:brand])
-
-    respond_to do |format|
-      if @brand.save
-        format.html { redirect_to(@brand, :notice => 'Brand was successfully created.') }
-        format.xml  { render :xml => @brand, :status => :created, :location => @brand }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @brand.errors, :status => :unprocessable_entity }
-      end
+    if @brand.save
+      redirect_to(@brand, :notice => '品牌新增成功！')
+    else
+      render :action => "new"
     end
   end
 
-  # PUT /brands/1
-  # PUT /brands/1.xml
   def update
     @brand = Brand.find(params[:id])
-
-    respond_to do |format|
-      if @brand.update_attributes(params[:brand])
-        format.html { redirect_to(@brand, :notice => 'Brand was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @brand.errors, :status => :unprocessable_entity }
-      end
+    if @brand.update_attributes(params[:brand])
+      redirect_to(@brand, :notice => '品牌更新成功！')
+    else
+      render :action => "edit"
     end
   end
 
--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,4 +1,6 @@
 class Brand < ActiveRecord::Base
   named_scope :name_contain, lambda {|name|{:conditions => ["brands.name LIKE ?", "%#{name}%"]}}
   named_scope :description_contain, lambda {|description|{:conditions => ["brands.description LIKE ?", "%#{description}%"]}}
+
+  validates_presence_of :name,:description
 end
