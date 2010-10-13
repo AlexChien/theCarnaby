f343a0eaff373d9ebc8cda93130eb48ab1f69ecf
lijian <jj365952505@hotmail.com>
2010-10-14 04:11:43 +0800

    new products

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,5 +1,6 @@
 class Brand < ActiveRecord::Base
   has_many :assets, :as=>:resource, :dependent=>:destroy
+  has_many :products
 
   named_scope :name_contain, lambda {|name|{:conditions => ["brands.name LIKE ?", "%#{name}%"]}}
   named_scope :description_contain, lambda {|description|{:conditions => ["brands.description LIKE ?", "%#{description}%"]}}
