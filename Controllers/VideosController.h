1e83f5a7d2b5e265067992898f30a24611e50410
stainless <johnsonqu@gmail.com>
2010-12-19 16:39:36 +0800

    video play

--- Controllers/VideosController.h
+++ Controllers/VideosController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 
-@interface VideosController : UIViewController {
+@interface VideosController : TTViewController {
   NSManagedObjectContext *managedObjectContext;
   NSManagedObjectContext *addingManagedObjectContext;
 }
--- Controllers/VideosController.m
+++ Controllers/VideosController.m
@@ -7,6 +7,7 @@
 //
 
 #import "VideosController.h"
+#import <MediaPlayer/MPMoviePlayerController.h>
 
 
 @implementation VideosController
@@ -27,16 +28,52 @@
 /*
 // Implement loadView to create a view hierarchy programmatically, without using a nib.
 - (void)loadView {
-
 }
 */
-
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
   NSLog(@"video view");
+  [self video_play:@"demo"];
 }
+
+- (void)playMovieAtURL:(NSURL*)theURL{
+  MPMoviePlayerController* theMovie= [[MPMoviePlayerController alloc] initWithContentURL:theURL];
+  theMovie.controlStyle = MPMovieControlStyleFullscreen;
+  theMovie.scalingMode=MPMovieScalingModeAspectFill;
+  //    theMovie.userCanShowTransportControls=NO;
+  
+  // Register for the playback finished notification.
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(myMovieFinishedCallback:)
+                                               name:MPMoviePlayerPlaybackDidFinishNotification
+                                             object:theMovie];
+  self.view = theMovie.view;
+  // Movie playback is asynchronous, so this method returns immediately.
+  [theMovie play];
+}
+
+// When the movie is done,release the controller.
+- (void)myMovieFinishedCallback:(NSNotification*)aNotification{
+  MPMoviePlayerController* theMovie=[aNotification object];
+  [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                  name:MPMoviePlayerPlaybackDidFinishNotification
+                                                object:theMovie];
+  
+  // Release the movie instance created in playMovieAtURL
+  [theMovie release];
+} 
+
+- (void)video_play:(NSString*)filename{
+  NSLog(@"filename: %@", filename);  
+  NSString* s = [[NSBundle mainBundle] pathForResource:filename ofType:@"mp4"];
+  NSLog(@"s: %@", s);   
+  NSURL* url = [NSURL fileURLWithPath:s];
+  NSLog(@"Playing URL: %@", url);
+  [self playMovieAtURL:url];
+}
+
 
 
 /*
