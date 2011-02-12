46f76785544e8420c5752b2cd2edd4765eb15a9f
stainless <johnsonqu@gmail.com>
2011-02-12 11:56:03 +0800

    rm mainwindow.xib

--- main.m
+++ main.m
@@ -11,7 +11,8 @@
 int main(int argc, char *argv[]) {
     
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    //int retVal = UIApplicationMain(argc, argv, nil, nil);
+    int retVal = UIApplicationMain(argc, argv, @"UIApplication", @"theCarnabyAppDelegate");
     [pool release];
     return retVal;
 }
