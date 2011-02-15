f912023d6513615cc0783943efdc1fa66df02146
stainless <johnsonqu@gmail.com>
2011-02-15 16:18:30 +0800

    web coupons show

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,7 +2,8 @@ class Coupon < ActiveRecord::Base
   belongs_to :product
   belongs_to :brand
   belongs_to :shop
-  
+  has_many :assets, :as=>:resource, :dependent=>:destroy
+
   validates_presence_of :name,:discount,:description,:name_en,:description_en#,:product_id,:shop_id
   
   validates_numericality_of :discount,:greater_than_or_equal_to=>0
